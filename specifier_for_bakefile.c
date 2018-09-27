26d3fd4adcdadba60da35dac572eb79bcecdfc60
hansel-nichola-santoso <43536693+hansel-nichola-santoso@users.noreply.github.com>
2018-09-27 09:36:36 +0800

    Create specifier_for_bakefile.c

---
+++ specifier_for_bakefile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unilib.h>
+
+
+comment()
+{
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+  FILE * open_bakefile = fopen("Bakefile.txt","r");
+
+
+  return 0;
+}
