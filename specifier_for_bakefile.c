d3dce3f39ba221ab137bdefea4041bc32241adbd
hansel-nichola-santoso <43536693+hansel-nichola-santoso@users.noreply.github.com>
2018-09-28 16:35:38 +0800

    Update specifier_for_bakefile.c

--- specifier_for_bakefile.c
+++ specifier_for_bakefile.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unilib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <limits.h>
 
 
-comment()
+
+bool comment(char * ch)
 {
+  char hashtag = '#';
+
+    if (ch[0] == hashtag)
+    {
+      return true;
+    }
+
+
+  return false;
+
 
 }
 
 
 int main(int argc, char const *argv[])
 {
-  FILE * open_bakefile = fopen("Bakefile.txt","r");
 
+  FILE * open_bakefile = fopen("Bakefile.txt","r+");
+  char fchar[4000];
+  fgets(fchar,sizeof(fchar),open_bakefile);
+
+
+      if (comment(fchar))
+      {
+        printf("It is a comment\n");
+      }
+      else
+      {
+        printf("It is not a comment");
+      }
 
   return 0;
 }
