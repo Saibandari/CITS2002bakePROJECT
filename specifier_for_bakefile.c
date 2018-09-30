55756671337e1cb2a8ff9108ca1fd81635489709
hansel-nichola-santoso <43536693+hansel-nichola-santoso@users.noreply.github.com>
2018-09-30 13:01:06 +0800

    Update specifier_for_bakefile.c
    This is for comment and as well as to let the program pick both Bakefile and bakefile

--- specifier_for_bakefile.c
+++ specifier_for_bakefile.c
@@ -1,43 +1,61 @@
 #include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <limits.h>
 
 
 
-bool comment(char * ch)
+
+void comment(FILE * open_bakefile)
 {
-  char hashtag = '#';
 
-    if (ch[0] == hashtag)
-    {
-      return true;
-    }
+  char counter[15000];
 
+  printf("This is the new line character\n");
+  FILE * buffer_file = fopen("Bufferfile.txt", "a");
 
-  return false;
-
+  while(1)
+  {
+    if(fgets(counter, sizeof(counter), open_bakefile)==NULL) break;
+      if (counter[0] != '#')
+      {
+        fprintf(buffer_file,"%s",counter);
+      }
+      else
+      {
+        continue;
+      }
+  }
 
 }
 
 
+
+
 int main(int argc, char const *argv[])
 {
+  FILE * open_bakefile = fopen("Bakefile", "r");
 
-  FILE * open_bakefile = fopen("Bakefile.txt","r+");
-  char fchar[4000];
-  fgets(fchar,sizeof(fchar),open_bakefile);
+  if(open_bakefile == NULL)
+  {
+    open_bakefile = fopen("bakefile" , "r");
+    comment(open_bakefile);
+    if (open_bakefile == NULL)
+    {
+      printf("No file can be used");
+    }
+  }
+  else
+  {
+    comment(open_bakefile);
+  }
 
 
-      if (comment(fchar))
-      {
-        printf("It is a comment\n");
-      }
-      else
-      {
-        printf("It is not a comment");
-      }
 
-  return 0;
+//printf("%i\n", n);
+fclose(open_bakefile);
+//printf("\n\nnth number of elements:  %i \n\n\n",line - 2);
+
+return 0;
 }
