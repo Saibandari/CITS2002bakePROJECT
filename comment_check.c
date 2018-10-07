cc639ec4cc3a9974ba82037051213482ba739049
yasomatinandana <39260702+yasomatinandana@users.noreply.github.com>
2018-10-07 12:00:42 +0800

    Add files via upload

---
+++ comment_check.c
@@ -0,0 +1,31 @@
+//
+// Created by yasom on 7/10/2018.
+//
+
+#include "comment_check.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+//The following function disregards any lines in Bakefile or bakefile that start with a #
+
+void comment_check(FILE * open_file)
+{
+    char buffer[15000];
+    FILE * buffer_file = fopen("Bufferfile.txt", "a");
+    while(1)
+    {
+        if(fgets(buffer, sizeof(buffer), open_file)==NULL) break;
+        if (buffer[0] != '#')
+        {
+            fprintf(buffer_file,"%s",buffer);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    fclose(buffer_file);
+}
+
---
+++ comment_check.h
@@ -0,0 +1,8 @@
+//
+// Created by yasom on 7/10/2018.
+//
+
+#ifndef BAKE_COMMENT_CHECK_H
+#define BAKE_COMMENT_CHECK_H
+
+#endif //BAKE_COMMENT_CHECK_H
