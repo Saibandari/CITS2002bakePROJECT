ba3a8e3e84260d96c0de3a3efb79111dca023e2e
hansel-nichola-santoso <43536693+hansel-nichola-santoso@users.noreply.github.com>
2018-10-08 14:47:56 +0800

    Add files via upload

--- bake.c
+++ bake.c
@@ -12,7 +12,13 @@
 //The following function is to extract the various text lines within Bakefile or bakefile
 
 
-int main()
+int main(int argc, char const *argv[])
 {
+	FILE * inputFILE = fopen("InputBuffer.txt","a"); // to store the arguments so then can be read using functions
+  for (int i = 1; i < argc; i++)
+  {
+    fprintf(inputFILE,"%s\t",argv[i]);
+  }
+	
 	read_bakefile();
 }
